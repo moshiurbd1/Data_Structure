2fe2d931dd4bde9018556e3c3e47c4a5248b487d
moshiurbd1 <moshiurbd101@gmail.com>
2025-01-22 21:29:51 +0600

    Range Sum Query using prefix sum

---
+++ Problems/Module 2.5_ Practice Day 01_solution/rangeSumQuery.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+long long int n,q;
+cin>>n>>q;
+int a[n],b[n];
+for(int i=0; i<n; i++){
+    cin>>a[i];
+}
+b[0]=a[0];
+for(int i=1; i<n; i++){
+    b[i]=b[i-1]+a[i];
+}
+
+while(q--){
+    long long l,r;
+    cin>>l>>r;
+    l--;
+    r--;
+    int sum=0;
+    if(l==0){
+        sum=b[r];
+    }else{
+        sum=b[r]-b[l-1];
+    }
+    cout<<sum<<endl;
+}
+
+
+return 0;
+}
+
